bb6f6e2e696c169a55a1ca6966641b4ab780e019
agungatidhira@Nyawaku <agungatidhira@gmail.com>
2018-07-24 13:19:06 +0700

    merevisi urutan per abjad

--- excercises-15-week-3.js
+++ excercises-15-week-3.js
@@ -1,23 +1,36 @@
 function groupAnimals(animals) {
   // you can only write your code here!
-  animals.sort();
+  // animals.sort();
   var arr = [];
+  var arrUnik = []
   var pembanding = animals[0][0];
   var arrBox = [];
-  for (var i = 0; i < animals.length; i++) {
-    if (animals[i][0] === pembanding) {
-      arrBox.push(animals[i]);
-    } else {
-      arr.push(arrBox);
-      arrBox = [];
-      pembanding = animals[i][0];
-      arrBox.push(animals[i]);
-    }
-    if (i === animals.length-1) {
-      arr.push(arrBox);
-    }
+
+  for ( var j=0; j < animals.length; j++) {
+    arr.push(animals[j].charAt(0))
+    // console.log(arr)
   }
-  return arr;
+  //loop arr, non Unik
+  var hurufUnik =''
+  arr.sort()
+  arrUnik.push(arr[0])
+    for ( var k = 1; k < arr.length-1; k++) {
+      if (arr[0] !== arr[k]) {
+        arrUnik.push(arr[k])
+      }    
+     } //console.log(arrUnik, k)
+  //looping animalas yg sesuai arrUnik
+  for (var i =0; i < arrUnik.length; i++) {
+     arrBox.push([])
+    for ( var j = 0; j < animals.length; j++) {
+      if ( arrUnik[i] === animals[j][0]) {
+        arrBox[i].push(animals[j])
+      }
+    }
+
+  } 
+ 
+  return arrBox;
 }
 
 // TEST CASES
